1c928a1a07864a176ec10e576f6b3317d64de181
Dtorres1ro <46894535+Datorresp@users.noreply.github.com>
2022-09-09 23:01:39 -0500

    Validation of a Llama Implemented

--- src/main/java/co/edu/icesi/CaliZoo/controller/AnimalController.java
+++ src/main/java/co/edu/icesi/CaliZoo/controller/AnimalController.java
@@ -3,10 +3,13 @@ package co.edu.icesi.CaliZoo.controller;
 import co.edu.icesi.CaliZoo.api.AnimalAPI;
 import co.edu.icesi.CaliZoo.dto.AnimalDTO;
 import co.edu.icesi.CaliZoo.mapper.AnimalMapper;
+import co.edu.icesi.CaliZoo.model.Animal;
 import co.edu.icesi.CaliZoo.service.AnimalService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -31,5 +34,66 @@ public class AnimalController implements AnimalAPI {
     @Override
     public List<AnimalDTO> getAnimals() {
         return animalService.getAnimals().stream().map(animalMapper::fromAnimal).collect(Collectors.toList());
+    }
+
+    private boolean checkName(String name){
+
+        boolean check = false;
+         if(name.length() <= 120){
+
+             //FALTA CHEQUEO ESPACIOS
+             if (name.matches("/^[A-Za-z\\s]+$/g")){
+
+                 check = true;
+             }
+         }
+        return check;
+    }
+
+    private boolean isLlama(AnimalDTO animalDTO){
+
+        boolean check = false;
+
+        //Maximum Llama Age is 28
+        if(animalDTO.getAge() >= 0 && animalDTO.getAge() <=  28){
+
+            if(animalDTO.getWeight() >= 10 && animalDTO.getWeight() <= 200) {
+
+                if (animalDTO.getHeight() >= 1.1  && animalDTO.getHeight() <= 1.5){
+
+                    check = true;
+                }
+            }
+        }
+
+        return check;
+    }
+
+    private boolean validateRepeatName(String name){
+
+        boolean check = true;
+        List<Animal> list = animalService.getAnimals();
+
+        for (int i = 0; i <= list.size(); i++){
+
+            if (name.equalsIgnoreCase(list.get(i).getName())){
+
+                check = false;
+            }
+        }
+
+        return check;
+    }
+
+    private boolean validateDate(Date date){
+
+        boolean check = true;
+        Date today = new Date();
+
+        if (date.after(today)){
+
+            check = false;
+        }
+        return check;
     }
 }
